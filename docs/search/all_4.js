92a192ed4f3849c732eb90e5fca07d7f9242637b
Alberto Niccolai <Alberto@AirdiAlbeberto.homenet.telecomitalia.it>
2024-12-07 11:40:45 +0100

    updated docs

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['main_0',['main',['../sim_8cc.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'sim.cc']]],
-  ['mu_20life_20simulation_1',['[ \mu ]-Life Simulation',['../index.html#autotoc_md1',1,'']]],
+  ['mu_20life_20simulation_1',['( \mu )-Life Simulation',['../index.html#autotoc_md1',1,'']]],
   ['myactioninitialization_2',['MyActionInitialization',['../class_my_action_initialization.html',1,'']]],
   ['mydetectorconstruction_3',['MyDetectorConstruction',['../class_my_detector_construction.html',1,'']]],
   ['myphysicslist_4',['MyPhysicsList',['../class_my_physics_list.html',1,'']]],
