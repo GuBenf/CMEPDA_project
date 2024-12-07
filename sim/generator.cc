85ab0b5095e18838b70850c1da13a956f8f15254
Guglielmo Benfratello <guglielmo.benfri121001@gmail.com>
2024-12-07 11:32:48 +0100

    100k mu-! Added pictures of analysis, customization in generator.cc, minor tweaks.

--- sim/generator.cc
+++ sim/generator.cc
@@ -19,7 +19,7 @@ void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
 {
   G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
   G4String particleName = "muon";
-  G4ParticleDefinition *particle = particleTable->FindParticle("mu+");
+  G4ParticleDefinition *particle = particleTable->FindParticle("mu-");
   fParticleGun->SetParticleDefinition(particle);
 
   // Generate a random position on a plane (e.g., square plane)
@@ -44,46 +44,68 @@ void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
   G4ThreeVector pos1(0., 1.*m, 0.);
   G4ThreeVector mom1(0., -1., 0.);
 
-  fParticleGun->SetParticlePosition(pos);
-  fParticleGun->SetParticleMomentumDirection(mom1);
+  // Choose the spatial distribution of the particle origin (set to true if you want all the particles over the center of the detector)
+  if (false){
+    fParticleGun->SetParticlePosition(pos1);
+  }
+  else {
+    fParticleGun->SetParticlePosition(pos);
+  }
 
+  // Choose the angular distribution of the particle direction (set to true if you want a uniform distribution in angle, else the trajectory will always be vertical)
+  if (false){
+    fParticleGun->SetParticleMomentumDirection(mom);
+  }
+  else {
+    fParticleGun->SetParticleMomentumDirection(mom1);
+  }
+
+
+  // Generate the momenta distribution (set to true if you want a Gaussian distribution, false if you want a "realistic" toy-distribution, feel free to play with it!)
+ 
+  if (true){
+  // Generate a Gaussian-distributed random number
   G4double mean = 80.0;    // Mean of the Gaussian distribution
   G4double sigma = 20.0;   // Standard deviation of the Gaussian distribution
-
-// Generate a Gaussian-distributed random number
   G4double u_mom = G4RandGauss::shoot(mean, sigma);
   G4double p_mom = u_mom * MeV;
-  // G4bool part = false;
-  // G4double p_m = 0.;
-  // G4int ind = 0;
-  // while (part == false)
-  //   {
-  //     ind = ind +1;
-  //     if (ind <4)
-  // 	{
-  // G4double u_mom = G4UniformRand();
-  // G4double p_mom = u_mom * 10.*GeV;
-  // if (p_mom > 1*GeV)
-  //   {
-  //     G4double cas = 1.*GeV/p_mom;
-  //     G4double p_sel = G4UniformRand();
-  //     if (p_sel<=cas)
-  // 	{
-  // 	  part = true;
-  // 	  p_m = p_mom;
-  // 	}
-  //   }
-  // else if (p_mom<=1.*GeV)
-  //   {
-  //     part = true;
-  //     p_m = p_mom;
-  //   }
-  // 	}
-  //     else
-  // 	{part=true;}
-  //   }
-
   fParticleGun->SetParticleMomentum(p_mom);
+
+  }
+
+  else{
+  G4bool part = false;
+  G4double p_m = 0.;
+  G4int ind = 0;
+  while (part == false)
+    {
+      ind = ind +1;
+      if (ind <4)
+	{
+  G4double u_mom = G4UniformRand();
+  G4double p_mom = u_mom * 10.*GeV;
+  if (p_mom > 1*GeV)
+    {
+      G4double cas = 1.*GeV/p_mom;
+      G4double p_sel = G4UniformRand();
+      if (p_sel<=cas)
+	{
+	  part = true;
+	  p_m = p_mom;
+	}
+    }
+  else if (p_mom<=1.*GeV)
+    {
+      part = true;
+      p_m = p_mom;
+    }
+	}
+      else
+	{part=true;}
+    }
+  fParticleGun->SetParticleMomentum(p_m);
+  }
+
 
   fParticleGun->GeneratePrimaryVertex(anEvent);
 
