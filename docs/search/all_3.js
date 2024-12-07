affe39297b094fffe84a46dd0e991f3e996d3c9d
Alberto Niccolai <Alberto@AirdiAlbeberto.homenet.telecomitalia.it>
2024-12-07 11:37:34 +0100

    updated docs

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['life_20simulation_0',['[\mu]-Life Simulation',['../index.html#autotoc_md1',1,'']]]
+  ['life_20simulation_0',['[ \mu ]-Life Simulation',['../index.html#autotoc_md1',1,'']]]
 ];
