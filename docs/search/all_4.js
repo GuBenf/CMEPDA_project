a1f902f69874784d3ed09b33b5b648b874122c84
Alberto Niccolai <Alberto@AirdiAlbeberto.homenet.telecomitalia.it>
2024-12-08 10:22:58 +0100

    Added docs for detector.*

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['detector_2ehh_0',['detector.hh',['../detector_8hh.html',1,'']]],
-  ['detectorlayer_1',['DetectorLayer',['../class_my_detector_construction.html#ad9960d816685b83c35a2cd7e809b733a',1,'MyDetectorConstruction']]],
-  ['documentation_2',['Documentation',['../index.html#autotoc_md2',1,'']]]
+  ['detector_2ecc_0',['detector.cc',['../detector_8cc.html',1,'']]],
+  ['detector_2ehh_1',['detector.hh',['../detector_8hh.html',1,'']]],
+  ['detectorlayer_2',['DetectorLayer',['../class_my_detector_construction.html#ad9960d816685b83c35a2cd7e809b733a',1,'MyDetectorConstruction']]],
+  ['documentation_3',['Documentation',['../index.html#autotoc_md2',1,'']]]
 ];
--- sim/detector.cc
+++ sim/detector.cc
@@ -1,38 +1,67 @@
+/**
+ * @file detector.cc
+ * @brief Implementation of the MySensitiveDetector class.
+ * 
+ * This file implements the functionality of the `MySensitiveDetector` class,
+ * which processes hits detected in sensitive volumes and records detector-specific
+ * data such as hits and timing information.
+ */
 #include "detector.hh"
 
+/**
+ * @brief Constructor for the `MySensitiveDetector` class.
+ * 
+ * Initializes the sensitive detector with a given name by calling the base class constructor.
+ * 
+ * @param name The name of the sensitive detector.
+ */
 MySensitiveDetector::MySensitiveDetector(G4String name) : G4VSensitiveDetector(name)
 {}
 
+/**
+ * @brief Destructor for the `MySensitiveDetector` class.
+ * 
+ * Cleans up any resources allocated by the sensitive detector.
+ */
 MySensitiveDetector::~MySensitiveDetector()
 {}
 
+/**
+ * @brief Processes hits detected in the sensitive detector.
+ * 
+ * This method is called whenever a particle interacts with the sensitive detector.
+ * It records relevant data, such as the detector's position, hit time, and particle's
+ * position, and assigns the recorded data to appropriate variables for further analysis.
+ * 
+ * @param aStep A pointer to the current step, containing information about the particle's movement.
+ * @param ROhist A pointer to the touchable history of the hit (not used in this implementation).
+ * 
+ * @return `true` if the hit was successfully processed.
+ */
 G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhist)
 {
+  // Obtain the track associated with the step and terminate it.
   G4Track *track = aStep->GetTrack();
-  track->SetTrackStatus(fStopAndKill);
+  track->SetTrackStatus(fStopAndKill); 
   
+  // Get pre-step and post-step points for positional information.
   G4StepPoint *preStepPoint = aStep->GetPreStepPoint();
   G4StepPoint *postStepPoint = aStep->GetPostStepPoint();
-
-  G4ThreeVector posPhoton = preStepPoint->GetPosition();
-
-  //G4cout << "Photon position: " << posPhoton << G4endl;
+  
+  // Access the touchable information to identify the detector.
   const G4VTouchable *touchable =aStep->GetPreStepPoint()->GetTouchable();
-
   G4int copyNo = touchable->GetCopyNumber();
-  //G4cout << "Copy number: " << copyNo << G4endl;
 
+  // Get the physical volume and its translation (detector position).
   G4VPhysicalVolume *physVol = touchable->GetVolume();
   G4ThreeVector posDetector = physVol->GetTranslation();
 
-  //G4cout<< "Detector Position: " << posDetector << G4endl;
-
+  // Retrieve and convert the global time of the hit to microseconds.
   G4double hittime = preStepPoint->GetGlobalTime();
-  G4double hitTime = hittime*0.001; //convert in us
-  //G4int det_1, det_2, det_3, det_4, det_5, det_6 = 0;
-  // G4double T_1,T_2,T_3,T_4,T_5,T_6 = 0; 
-  //std::cout<< posDetector[1]<<std::endl;
-    
+  G4double hitTime = hittime * 0.001; // Convert to microseconds.
+  
+  // Determine the detector region based on the Y-coordinate of the detector's position.
+  // Assign hits and timing values accordingly.  
   if(posDetector[1] <= (10.+0.5)*10 && posDetector[1] >= (10.-0.5)*10)
   {
 	    set_det_1(1);
@@ -130,13 +159,14 @@ G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhis
 	    set_t_6(hitTime);
   }
 
-  //G4cout << "Detector: "<< det_1 << " " << det_2 << " " << det_3 << " " << det_4 << " " << det_5 << " " << det_6 << G4endl;
-  //G4cout << "Hittime: " << t_1 << " " << t_2 << " " << t_3 << " " << t_4 << " " << t_5 << " " << t_6 << G4endl;
+  // Retrieve the current event ID.
   G4int evt = G4RunManager::GetRunManager()->GetCurrentRun()->GetRunID();
   
+  // Access the analysis manager to fill the N-tuple with data.
   G4AnalysisManager *man = G4AnalysisManager::Instance();
   man->FillNtupleIColumn(0,0,evt);
   
+  // Record hits for all detectors.
   man->FillNtupleIColumn(0,1,get_det_1());
   man->FillNtupleIColumn(0,2,get_det_2());
   man->FillNtupleIColumn(0,3,get_det_3());
@@ -151,5 +181,7 @@ G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhis
   man->FillNtupleDColumn(0,11,get_t_5());
   man->FillNtupleDColumn(0,12,get_t_6());
   man->FillNtupleDColumn(0,13,hitTime);
+
+  // Save the row in the N-tuple.
   man->AddNtupleRow(0);
 }
